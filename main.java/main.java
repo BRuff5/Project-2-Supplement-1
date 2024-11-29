f3a0e3f607a6ade05dbb1ae1cb483ea95c704b02
BLR <bruff@Macky-Max.local>
2024-11-28 21:34:05 -0500

    this feature inserts a new value and if the value is smaller inserts left bigger inserts to the right

--- main.java/main.java
+++ main.java/main.java
@@ -20,4 +20,32 @@ public class main {
         this.left = null;
         this.right = null;
     }
+
+    /**
+     * Inserts a new value ]]
+     * If value is smaller than the current data, its inserted into the left child.
+     * If bigger inserted into the right child.
+     *
+     * @param newData The value to insert.
+     */
+
+    public void insert(int newData) {
+        if (newData < this.data) {
+            // Insert into the left 
+            if (this.left == null) {
+                // Create a new node if left child does not exist
+                this.left = new main(this.id * 2, newData); 
+            } else {
+                this.left.insert(newData);
+            }
+        } else {
+            // Insert into the right child
+            if (this.right == null) {
+                // Create a new node if right child does not exist
+                this.right = new main(this.id * 2 + 1, newData); 
+            } else {
+                this.right.insert(newData);
+            }
+        }
+    }
 }
